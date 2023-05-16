0393e193559cc9abc356519e3e42bb1f3354b857
efinlaw <47069204+efinlaw@users.noreply.github.com>
2023-05-15 17:47:35 -0700

    modified webpack configuration to switch from using dev server to watch. Added an entry point for init module.

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: '.dist',
+  entry: {
+    index: './src/index.js',
+    init: './src/init.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Bub\'s Bakery',
+      template: './src/index.html',
+      inject: 'body',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
